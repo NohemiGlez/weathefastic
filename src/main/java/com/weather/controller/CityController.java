9fab2f7703e3e01e14122b8a240025422b821cba
NohemiGlez <ngonzalez.swe@gmail.com>
2019-03-11 19:24:51 -0700

    Pass variable 'cityName' from response to weather view

--- src/main/java/com/weather/controller/CityController.java
+++ src/main/java/com/weather/controller/CityController.java
@@ -1,6 +1,7 @@
 package main.java.com.weather.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,29 +10,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import main.java.com.weather.model.City;
-import main.java.com.weather.model.FiveDaysWeatherForecast;
+import main.java.com.weather.model.DayForecast;
 import main.java.com.weather.util.OpenWeatherAPIConfig;
 
 @RestController
 public class CityController {
 
 	@RequestMapping(value = "/{city}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-	private @ResponseBody FiveDaysWeatherForecast getCityByURLParam(@PathVariable("city") String city) {
+	private @ResponseBody DayForecast getCityByURLParam(@PathVariable("city") String city) {
 
 		String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
 
 		RestTemplate restTemplate = new RestTemplate();
-		FiveDaysWeatherForecast response = restTemplate.getForObject(URL, FiveDaysWeatherForecast.class);
+		DayForecast response = restTemplate.getForObject(URL, DayForecast.class);
 		return response;
 	}
 
 	@RequestMapping(value = "/weather", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-	private @ResponseBody FiveDaysWeatherForecast getCityByPassedParam(@RequestParam(value = "city") String city) {
+	private @ResponseBody Model getCityByPassedParam(@RequestParam(value = "city") String city, Model model) {
 		String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
 
 		RestTemplate restTemplate = new RestTemplate();
-		FiveDaysWeatherForecast response = restTemplate.getForObject(URL, FiveDaysWeatherForecast.class);
-		return response;
+		DayForecast response = restTemplate.getForObject(URL, DayForecast.class);
+		return model.addAttribute("cityName", response.getCity().getName());
 	}
 }
