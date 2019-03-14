b21494edd2fe2da7bee79718e566515b298621c9
NohemiGlez <ngonzalez.swe@gmail.com>
2019-03-13 19:02:08 -0700

    Add class in order to ignore SSL certificate in url API access

--- src/main/java/com/weather/controller/WeatherController.java
+++ src/main/java/com/weather/controller/WeatherController.java
@@ -1,17 +1,21 @@
 package main.java.com.weather.controller;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClientException;
 
 import main.java.com.weather.model.Response;
 import main.java.com.weather.util.OpenWeatherAPIConfig;
+import main.java.com.weather.util.RestTemplateMethods;
 
 @RestController
 public class WeatherController {
@@ -19,19 +23,40 @@ public class WeatherController {
 	@RequestMapping(value = "/{city}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
 	private @ResponseBody Response getCityByURLParam(@PathVariable("city") String city) {
 
-		String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
-
-		RestTemplate restTemplate = new RestTemplate();
-		Response response = restTemplate.getForObject(URL, Response.class);
-		return response;
+		try {
+			String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
+			Response response = RestTemplateMethods.getRestTemplate().getForObject(URL, Response.class);
+			return response;
+		} catch (RestClientException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException e) {
+			e.getStackTrace().toString();
+		}
+		return null;
 	}
 
-	@RequestMapping(value = "/weather", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-	private @ResponseBody Model getCityByPassedParam(@RequestParam(value = "city") String city, Model model) {
-		String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
+	@RequestMapping(value = "/weather", method = RequestMethod.GET)
+	private @ResponseBody void getCityByPassedParams(@PathVariable("city") String city, Model model) {
 
-		RestTemplate restTemplate = new RestTemplate();
-		Response response = restTemplate.getForObject(URL, Response.class);
-		return model.addAttribute("cityName", response.getCity().getName());
+		try {
+			String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
+			Response response = RestTemplateMethods.getRestTemplate().getForObject(URL, Response.class);
+			model.addAttribute("date", response.getCity().toString());
+		} catch (RestClientException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException e) {
+			e.getStackTrace().toString();
+		}
+//		return null;
 	}
+
+//	@RequestMapping(value = "/weather", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+//	private @ResponseBody void getCityByPassedParam(@RequestParam(value = "city") String city, Model model) {
+//		String URL = OpenWeatherAPIConfig.getCityInformationURL(city);
+//
+//		RestTemplate restTemplate = new RestTemplate();
+//		Response response = restTemplate.getForObject(URL, Response.class);
+//		for (Forecast forecast : response.getList()) {
+//			String dt_txt = (forecast.getDt_txt());
+//			if (dt_txt != null) {
+//				model.addAttribute("date", dt_txt.toString());
+//			}
+//		}
+//	}
 }
